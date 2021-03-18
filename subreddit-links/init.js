a677833b956aca20bacd1c88a8cd8cf231227853
Job Bautista <jobbautista9@protonmail.com>
2021-03-18 21:29:19 +0800

    Quality-of-life change for subreddit-links
    Should now accept r/, user/, and u/
    No need to prepend a forward slash

--- subreddit-links/init.js
+++ subreddit-links/init.js
@@ -15,9 +15,9 @@ plugin.init = function(glob)
 
 plugin.enable = function()
 {
-	client.munger.addRule("subreddit-links", /(\/(r|user)\/([A-Za-z0-9]{3,}))/, function(matchText, containerTag, eventData, mungerEntry){
+	client.munger.addRule("subreddit-links", /((r|u|user)\/([A-Za-z0-9]{3,}))/, function(matchText, containerTag, eventData, mungerEntry){
 		var newLink = document.createElementNS(XHTML_NS, "html:a");
-		newLink.setAttribute("href", "http://www.reddit.com/" + matchText.substr(1));
+		newLink.setAttribute("href", "http://www.reddit.com/" + matchText.substr(0));
 		newLink.setAttribute("location", "_blank");
 		newLink.appendChild(document.createTextNode(matchText));
 		containerTag.appendChild(newLink);
